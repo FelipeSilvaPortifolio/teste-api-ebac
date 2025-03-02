db9c92e9b0938e21b1a9138a2d4f2edca9d5adb4
FelipeSilvaPortifolio <portifoliofelipesilva@gmail.com>
2025-03-02 13:34:55 -0300

    Update Cypress configuration to set default command and response timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000/",
+    "defaultCommandTimeout": 10000,
+    "responseTimeout": 15000,
+    "baseUrl": "http://localhost:3000"
   },
 });
